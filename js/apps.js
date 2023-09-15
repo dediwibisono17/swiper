7f91a1f8532a837fc4b2ce1ac9528f19499cd5bd
dediwibisono17 <dedi.wibisono17@gmail.com>
2023-09-15 13:22:09 +0700

    ded

--- js/apps.js
+++ js/apps.js
@@ -35,7 +35,7 @@ setTimeout(function () {
 
 $(function () {
   // $(".swiper-slide .wrap").attr({ "data-aos": "fade-in" })
-  $(".swiper-slide .wrap").attr({ "data-aos": "fade-down", "data-aos-delay": "500", "data-aos-duration": "700" })
+  $(".swiper-slide .wrap").attr({ "data-aos": "fade-right", "data-aos-delay": "200", "data-aos-duration": "500" })
   // $(".swiper-slide .wrap").removeClass('aos-animate')
 
   var swiper = new Swiper('.swiper-container', {
